93be8acf9914544ac54f389a88e51391bbbaf0d7
Tom Ritchford <tom@swirly.com>
2016-07-09 13:47:00 -0400

    increment

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 import platform, setuptools, setuptools, Cython.Build
 
 EXTENSION = setuptools.extension.Extension(
-    name='simple',
-    sources=['simple.pyx'],
-    include_dirs = ['subd'],
+    name='sub',
+    sources=['src/sub.pyx'],
+    include_dirs=['src'],
     extra_compile_args=['-Wno-unused-function', '-std=c++11',
                         '-mmacosx-version-min=10.9',
                         ],
--- simple.pyx
+++
@@ -1,4 +0,0 @@
-include "sub1.pyx"
-
-# What settings can I put in setup.py to be able to uncomment the next line?
-# include "sub2.pyx"
